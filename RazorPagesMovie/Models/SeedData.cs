c63379e306d1a0dbf7a38184aeeac6c575fde362
Helen Kalamäe <hekala@ttu.ee>
2020-02-09 18:20:32 +0200

    Homework1

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Alice",
+                        ReleaseDate = DateTime.Parse("2000-2-11"),
+                        Genre = "Romantic Comedy",
+                        Price = 2M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghosts ",
+                        ReleaseDate = DateTime.Parse("1999-3-7"),
+                        Genre = "Horror",
+                        Price = 8M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghosts 2",
+                        ReleaseDate = DateTime.Parse("2002-2-23"),
+                        Genre = "Horror",
+                        Price = 9M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Winter",
+                        ReleaseDate = DateTime.Parse("1983-4-15"),
+                        Genre = "Drama",
+                        Price = 3M,
+                        Rating = "G"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        [BindProperty(SupportsGet = true)]//binds form values and query strings w the same name
+        public string SearchString { get; set; }
+       
+        public SelectList Genres { get; set; } //allows user to select a genre from the list
+        [BindProperty(SupportsGet = true)] 
+        public string MovieGenre { get; set; } //contains specific genre user selects
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;  //using LINQ to get genres
+            var movies = from m in _context.Movie //creates a linq query to select the movies, searches movies from database
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()); //projects a list of diff genres
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
